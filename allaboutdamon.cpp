d566e3aa4686d4ab20f9069e4a7e25e00b55faa5
Damon Diulio <289991@burlcoschools.org>
2026-02-12 08:44:12 -0500

    Add basic information display in allaboutdamon.cpp

---
+++ allaboutdamon.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+//code
+int main()
+{
+    cout <<"       Damon Diulio\n       609 342 7522\n    Computer Engineering\n 14 Erik Dr Browns Mills Nj 08015 ";
+}
