faf83179c050cbd1e03a3b55e6e6d78fddde7798
Damon Diulio <289991@burlcoschools.org>
2026-02-13 10:35:23 -0500

    Modify main function to show personal details
    Updated the main function to display personal information.

--- allaboutdamon.cpp
+++ allaboutdamon.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-//code
+//Displaying the information about me
 int main()
 {
     cout <<"       Damon Diulio\n       609 342 7522\n    Computer Engineering\n 14 Erik Dr Browns Mills Nj 08015 ";
